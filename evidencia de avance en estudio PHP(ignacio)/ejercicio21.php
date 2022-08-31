1ab3200956c18423e76ab7dac7c39478c64f4c32
Ignacio987 <ignxcioxguilxr@gmail.com>
2022-08-31 13:53:05 -0400

    primer avance del estudio PHP

---
+++ evidencia de avance en estudio PHP(ignacio)/ejercicio21.php
@@ -0,0 +1,9 @@
+<?php
+//ARREGLO DE ÍNDICE NUMÉRICO
+// Array ( [0] => fresa [1] => peras [2] => manzana [3] => plátano )
+$frutas = array("fresa", "peras", "manzana", "plátano");
+//imprime Array ( [0] => fresa [1] => peras [2] => manzana [3] => plátano )
+print_r($frutas);
+// imprime el contenido de $frutas[1], en este caso peras
+echo "<br>".$frutas[1];
+?>
