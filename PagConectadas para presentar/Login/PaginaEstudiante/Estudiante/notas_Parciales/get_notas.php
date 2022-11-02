424ef54a5b8e23885fdcf93891234b50161471f0
ctt221 <106630043+ctt221@users.noreply.github.com>
2022-11-02 00:15:32 -0300

    Add files via upload

---
+++ PagConectadas para presentar/Login/PaginaEstudiante/Estudiante/notas_Parciales/get_notas.php
@@ -0,0 +1,18 @@
+<?php
+    include('conexion.php');
+
+    $curso = isset($_POST['curso']) ? $_POST['curso'] : 'vacio';
+
+
+    $con = conectar();
+    $sql3="SELECT * FROM `$curso` ";  
+
+    $res3=mysqli_query($con,$sql3) or die ('Error en el query database');
+
+
+//Valida que la consulta esté bien hecha
+    while($row = $res3->fetch_assoc()) {
+        $myArray= $row;
+    }
+
+    echo json_encode($myArray);
